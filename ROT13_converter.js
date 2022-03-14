08f085a0cbed7eb6ee1e8714627765263b3bf6c7
Bjørnar Borge <72221999+bjbogithub@users.noreply.github.com>
2022-03-14 09:39:26 +0100

    Update ROT13_converter.js

--- ROT13_converter.js
+++ ROT13_converter.js
@@ -1,44 +1,19 @@
-function rot13() {
-    var letters = {
-        'N':'A',
-        'O':'B',
-        'P':'C',
-        'Q':'D',
-        'R':'E',
-        'S':'F',
-        'T':'G',
-        'U':'H',
-        'V':'I',
-        'W':'J',
-        'X':'K',
-        'Y':'L',
-        'Z':'M',
-        'A':'N',
-        'B':'O',
-        'C':'P',
-        'D':'Q',
-        'E':'R',
-        'F':'S',
-        'G':'T',
-        'H':'U',
-        'I':'V',
-        'J':'W',
-        'K':'X',
-        'L':'Y',
-        'M':'Z',
-        '?':'?',
-        '!':'!',
-        '.':'.',
-        ' ':' '
-    };
-    var strOut = '';
-    var newStr;
-    var str = document.getElementById("input").value;
-    for(var i =0; i < str.length; i++){
-      newStr = letters[str[i]];
-      strOut += newStr;
-    }
-    document.getElementById('output').innerHTML = strOut;
-}
+var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M'];
+var oppAlph = ['N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
+function rot13() {
+  var str = document.getElementById("input").value;
+  var newStr = str.split("");
+  for (let i = 0; i < newStr.length; i++) {
+    for (let j = 0; j < alphabet.length; j++) {
+      if (newStr[i] === alphabet[j]) {
+        newStr[i] = oppAlph[j];
+      } else if (newStr[i] === oppAlph[j]) {
+        newStr[i] = alphabet[j];
+      }
+    }
+  }
+  document.getElementById("output").innerHTML = newStr.join("");
+
+}
 
