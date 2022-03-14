e88ab2d5351b79bbf9a81dcb67f42cafddc75143
Bjørnar Borge <72221999+bjbogithub@users.noreply.github.com>
2022-03-14 08:07:19 +0100

    Update ROT13_converter.js

--- ROT13_converter.js
+++ ROT13_converter.js
@@ -1,45 +1,17 @@
-function rot13(str) {
-  
-    //Dictionary of values
-    const key = {
-      'N' : 'A',
-      'O' : 'B',
-      'P' : 'C',
-      'Q' : 'D',
-      'R' : 'E',
-      'S' : 'F',
-      'T' : 'G',
-      'U' : 'H',
-      'V' : 'I',
-      'W' : 'J',
-      'X' : 'K',
-      'Y' : 'L',
-      'Z' : 'M',
-      'A' : 'N',
-      'B' : 'O',
-      'C' : 'P',
-      'D' : 'Q',
-      'E' : 'R',
-      'F' : 'S',
-      'G' : 'T',
-      'H' : 'U',
-      'I' : 'V',
-      'J' : 'W',
-      'K' : 'X',
-      'L' : 'Y',
-      'M' : 'Z'
-    }
-    
-    //Convert string to array
-    let stringArray = str.split("");
-  
-    //function for replacing using the dictionary
-    function trans(array){
-      return [].reduce.call(array, (transcribed, char, index) => {
-        return transcribed +=  (key[char] || char);
-        },'');
-    }
-  
-    let tranString = trans(stringArray);
-    return tranString;
+function rot13() {
+  let str = document.getElementById("input").value;
+  let lettersA2M = "ABCDEFGHIJKLM";
+  let lettersN2Z = "NOPQRSTUVWXYZ";
+  let decodedString = "";
+  for (let i = 0; i < str.length; i++) {
+      if (str[i].match(/[A-M]/)) {
+          decodedString += lettersN2Z[lettersA2M.indexOf(str[i])];
+      } else if (str[i].match(/[N-Z]/)) {
+          decodedString += lettersA2M[lettersN2Z.indexOf(str[i])];
+      } else {
+          decodedString += str[i];
+      }
   }
+  let palindrome_answer2= decodedString;
+  document.getElementById('output').innerHTML = palindrome_answer2;
+}
